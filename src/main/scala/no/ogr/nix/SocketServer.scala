1eafe49708da9ea879d05a67edc588e7b7e7bdf0
Øyvind Grønnesby <ogr@ogr.no>
2014-03-09 22:40:21 +0100

    Rename pipeline factory

--- src/main/scala/no/ogr/nix/SocketServer.scala
+++ src/main/scala/no/ogr/nix/SocketServer.scala
@@ -3,17 +3,16 @@ package no.ogr.nix
 import java.net.InetSocketAddress
 import java.nio.channels.{SocketChannel, SelectionKey, ServerSocketChannel}
 import rx.lang.scala.{Observer, Observable}
-import no.ogr.nix.SocketServer.PipelineFactory
+import no.ogr.nix.SocketServer.ClientHandler
 
 class SocketServer(eventLoop: EventLoop,
                    channel: ServerSocketChannel,
-                   clientPipeline: PipelineFactory)
-{
+                   clientPipeline: ClientHandler) {
   channel.configureBlocking(false)
 
   private val acceptObserver = new Observer[Event[ServerSocketChannel]] {
     override def onNext(event: Event[ServerSocketChannel]) {
-      val channel    = event.channel.accept()
+      val channel = event.channel.accept()
       channel.configureBlocking(false)
       val observable = event.service.select(channel, SelectionKey.OP_READ)
       clientPipeline(observable)
@@ -23,9 +22,9 @@ class SocketServer(eventLoop: EventLoop,
 }
 
 object SocketServer {
-  type PipelineFactory = (Observable[Event[SocketChannel]]) => Unit
+  type ClientHandler = (Observable[Event[SocketChannel]]) => Unit
 
-  def apply(eventLoop: EventLoop, address: InetSocketAddress, backlog: Int = 0)(factory: PipelineFactory): SocketServer = {
+  def apply(eventLoop: EventLoop, address: InetSocketAddress, backlog: Int = 0)(factory: ClientHandler): SocketServer = {
     val channel = ServerSocketChannel.open().bind(address, backlog)
     new SocketServer(eventLoop, channel, factory)
   }
